23da661d4a48ce53f3ed49aa8e01c43a6cb08553
vmonge79 <vivi_monge@hotmail.com>
2024-09-26 19:07:57 -0600

    dom 01

---
+++ week02/scripts/boom-script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = 'X';
+
+li.append(deleteButton);
+
+list.append(li);
+
