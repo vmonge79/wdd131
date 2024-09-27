f65d71a95f410cfa054e1e393b062b3bdc11d73f
vmonge79 <vivi_monge@hotmail.com>
2024-09-27 15:39:15 -0600

    temple 03

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,18 @@
 const year = document.querySelector("#year");
 const today = new Date();
 
-year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
-
+year.textContent = today.getFullYear();
 
 const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = new Date(document.lastModified);
 const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}`;
 lastModifiedElement.textContent = `Last modified: ${formattedDate}`;
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
