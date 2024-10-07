89cd50e1dab30fd824782b8a19f166ed00ce4cc3
vmonge79 <vivi_monge@hotmail.com>
2024-10-07 14:43:31 -0600

    place07

--- scripts/place.js
+++ scripts/place.js
@@ -8,3 +8,31 @@ const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = new Date(document.lastModified);
 const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}`;
 lastModifiedElement.textContent = `Last modified: ${formattedDate}`;
+
+function calculateWindChill(){
+    const tempElement = document.querySelector(".temp");
+    const windelement = document.querySelector(".wind");
+    const windChillElement = document.querySelector(".windchill");
+
+    const temperature = parseFloat(tempElement.textContent);
+    const windSpeed = parseFloat(windelement);
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill = (    
+            13.12 +
+            0.6215 * temperature -
+            11.37 * Math.pow(windSpeed, 0.16) +
+            0.3965 * temperature * Math.pow(windSpeed, 0.16)
+        ).toFixed(2);
+
+        windChillElement.innerHTML = `${windChill}ºC`;
+    } else {
+        windChillElement.innerHTML = "N/A";
+    }
+
+calculaleWindChill();
+
+
+
+
+}
