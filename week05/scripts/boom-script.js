9890d72a4c2ddd5ba0bc55e1aeaca354065bf4e9
vmonge79 <vivi_monge@hotmail.com>
2024-10-17 15:03:18 -0600

    form01

---
+++ week05/scripts/boom-script.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+let chaptersArray = getChapterList() || [];
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = 'X';
+
+li.append(deleteButton);
+
+list.append(li);
+
+button.addEventListener('click', function () {
+    if (input.value != '') {
+        displayList(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+
+
