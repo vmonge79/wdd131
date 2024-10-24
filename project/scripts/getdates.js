25947c8b937a98d24b309ae5c7427281194a640e
vmonge79 <vivi_monge@hotmail.com>
2024-10-23 18:26:58 -0600

    project2310

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -8,3 +8,12 @@ const lastModifiedElement = document.querySelector("#lastModified");
 const lastModified = new Date(document.lastModified);
 const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}`;
 lastModifiedElement.textContent = `Last modified: ${formattedDate}`;
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
